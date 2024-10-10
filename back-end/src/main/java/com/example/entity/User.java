a4acf8ee5ff16b8b4bf28288147455353d0c70fc
liuyihan <e1350027@u.nus.edu>
2024-10-10 22:30:20 +0800

    usermanage

---
+++ back-end/src/main/java/com/example/entity/User.java
@@ -0,0 +1,85 @@
+package com.example.entity;
+
+public class User extends Account {
+
+    /** ID */
+    private Integer id;
+    /** 用户名 */
+    private String username;
+    /** 密码 */
+    private String password;
+    /** 姓名 */
+    private String name;
+    /** 电话 */
+    private String phone;
+    /** 邮箱 */
+    private String email;
+    /** 头像 */
+    private String avatar;
+    /** 角色标识 */
+    private String role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
